0661be332b49d60231ad7b33b98c34195b5c9c02
MarkGodnyi <libertyinprogresslife@gmail.com>
2024-06-14 18:31:51 +0200

    added Person component

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,20 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+export const Person = ({ person }) => {
+  let personPartnerPhrase = 'I am not married';
+  const personPartner = person.sex === 'm' ? 'wife' : 'husband';
+
+  if (person.isMarried) {
+    personPartnerPhrase = `${person.partnerName} is my ${personPartner}`;
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {person.name}</h2>
+
+      {person.age > 0 && <p className="Person__age">I am {person.age}</p>}
+
+      <p className="Person__partner">{personPartnerPhrase}</p>
+    </section>
+  );
+};
